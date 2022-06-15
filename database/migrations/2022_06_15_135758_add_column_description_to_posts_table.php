e77090deb29abe0df34e5358c210db2fee583c67
malakmadze <malakmadzesergo@gmail.com>
2022-06-15 17:23:54 +0300

    3.migrate_CRUD

---
+++ database/migrations/2022_06_15_135758_add_column_description_to_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+
+            #Create Columnt Description after the column Content
+            $table->text('description')->nullable()->after('content');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+
+            # To drop created column when needed
+            $table->dropColumn('description');
+
+        });
+    }
+};

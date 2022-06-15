d6f0a9ba4a1f64117d4b25a2f597cbcd43588f45
malakmadze <malakmadzesergo@gmail.com>
2022-06-15 18:50:00 +0300

    4.view

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class ContactsController extends Controller
+{
+    #Read
+    public function index()
+    {
+        $posts = Post::all();
+        return view('contacts');
+    }
+}

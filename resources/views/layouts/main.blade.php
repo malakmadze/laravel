20e605e6049685b07dd2404face6d8b2623e8afc
malakmadze <malakmadzesergo@gmail.com>
2022-06-16 12:19:47 +0300

    5.view + bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,18 +5,32 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Document</title>
 </head>
 <body>
-<div>
-    <div>
-        <nav>
-            <ul>
-                <li><a href="{{route('main.index')}}">Main</a> </li>
-                <li><a href="{{route('post.index')}}">Posts</a> </li>
-                <li><a href="{{route('about.index')}}">About</a> </li>
-                <li><a href="{{route('contacts.index')}}">Contacts</a> </li>
-            </ul>
+<div class="container">
+    <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{route('main.index')}}">Main <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('post.index')}}">Posts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('about.index')}}">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('contacts.index')}}">Contacts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     </div>
     @yield('content')
